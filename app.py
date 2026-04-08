d9ceca523b519d2bd5d7e399fd1b43a575b7ec9e
Sam-168 <samukelondlela2.0@gmail.com>
2026-04-08 04:56:57 +0200

    initialized FastAPI app and added CORS middleware

--- app.py
+++ app.py
@@ -9,3 +9,17 @@ from io import BytesIO
 import cv2
 
 from face_utils import FaceRecognitionService
+
+app = FastAPI(
+    title="Face Recognition Service",
+    description="Face recognition API for student attendance system",
+    version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #spring backend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
